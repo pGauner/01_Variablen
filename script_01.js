0f3fbe14d961c3e9e7937b034397953a46a9e72a
Ozan Oezyurt <ozan.oezyurt@icloud.com>
2023-05-09 11:21:29 +0200

    ++commit

--- script_01.js
+++ script_01.js
@@ -9,9 +9,40 @@
 */
 
 // Konsolen-Ausgaben
-// console.log("Hi");
-// console.log(firstName);
+// console.log("Hello, World!");
+
+// console.log("firstName");
 
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
+// let firstName; // Deklaration (Definition)
+// firstName = "Ozan"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+// let familyName; // Deklaration
+// familyName = "Oezyurt"; // Wertzuweisung
+// console.log(familyName); // Ausgabe
+
+/*****  03 Deklaration + Wertzuweisung II *****/
+
+// let firstName, familyName
+
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bite Nachnamen eingeben:");
+
+// console.log(firstName +" "+ familyName);
+// console.log(typeof firstName);
+
+/* JS ist eine untypisierte Sprache! | untyped */
+
+let test; 
+test = "Ozan"; // string
+test = 2; // number
+test = true; // boolean
+
+
+
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
